18b0029421ff2a581bdcd5493062d7f4f5718be4
ertugrulsaliher <ertugrulsaliher@gmail.com>
2023-08-22 21:23:22 +0300

    initial commit

--- src/Giris/FaktoriyelHesaplamaVeKombinasyonOdevi.java
+++ src/Giris/FaktoriyelHesaplamaVeKombinasyonOdevi.java
@@ -4,6 +4,7 @@ public class FaktoriyelHesaplamaVeKombinasyonOdevi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        /*
         int i=1;
         int Faktoriyel = 1;
 
@@ -16,5 +17,42 @@ public class FaktoriyelHesaplamaVeKombinasyonOdevi {
             }
 
             System.out.println(Sayi + "! = " + Faktoriyel);
+
+            C(n,r) = n! / (r! * (n-r)!)
+
+         */
+
+
+        System.out.print("n Sayısını Seçin :");
+        int nSayisi = input.nextInt();
+
+        System.out.print("r Sayısını Seçin :");
+        int rSayisi = input.nextInt();
+
+        int i = 1;
+        int k = 1;
+        int l = 1;
+        int nEksir = nSayisi-rSayisi;
+        int NFaktoriyel =1;
+        int RFaktoriyel =1;
+        int nEksirFaktoriyel =1;
+
+        for (i=1; i<=nSayisi; i++) {
+            NFaktoriyel = NFaktoriyel * i;
+        }
+
+        for (k=1; k<=rSayisi; k++) {
+            RFaktoriyel = RFaktoriyel *k;
+        }
+
+        for (l=1; l<=nEksir; l++) {
+            nEksirFaktoriyel = nEksirFaktoriyel *l;
+        }
+
+
+        System.out.print("Sonuç = ");
+        System.out.print(NFaktoriyel/(RFaktoriyel*nEksirFaktoriyel));
+
+
     }
 }
